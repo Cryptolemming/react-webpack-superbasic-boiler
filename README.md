3c3badd3a1fe99fca5d0aa0376956c2610cb37a2
cryptolemming <aliayoub22@gmail.com>
2016-02-24 12:49:29 -0500

    now functioning

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# athingaweek
-Exercises done for [athingaweek.com](http://www.athingaweek.com)
+# Basic React / Webpack / Node / Express boiler
+
+clone
+```
+git clone
+```
+
+install repositories
+```
+npm install
+```
+
+run (use nodemon for continuous)
+```
+node app.js 
+``
+
+to build (--watch flag for continuous)
+```
+webpack --watch
+```
+
---
+++ index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Radium from 'radium';
+
+'use strict';
+
+var styles = {
+	example: {
+		color: 'blue',
+	}
+};
+
+var App = Radium(React.createClass({
+	
+	render() {
+
+		return(
+			<div>
+				<h1 style={styles.example}>
+					I am functioning properly
+				</h1>
+			</div>
+		)
+	}
+}));
+
+ReactDOM.render(<App />, document.getElementById('container'));
